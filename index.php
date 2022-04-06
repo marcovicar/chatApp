762a7169d1d4ef50cf98be46aa5765f8417f2283
marcovicar <66976105+marcovicar@users.noreply.github.com>
2022-04-06 17:37:21 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!-- Coding by Marcovicar - github.com/marcovicar -->
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Now Chat App | Marcovicar</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Now Chat App</header>
+            <form action="#">
+                <div class="error-txt">This is an error message!</div>
+                <div class="name-details">
+                    <div class="field">
+                        <label>Nome</label>
+                        <input type="text" name="firstname" placeholder="Nome">
+                    </div>
+                    <div class="field">
+                        <label>Sobrenome</label>
+                        <input type="text" name="lastname" placeholder="Sobrenome">
+                    </div>
+                </div>
+                    <div class="field">
+                        <label>Email</label>
+                        <input type="text" name="email" placeholder="Digite o seu email">
+                    </div>
+                    <div class="field">
+                        <label>Senha</label>
+                        <input type="text" name="password" placeholder="Digite uma senha">
+                    </div>
+                    <div class="field">
+                        <label>Selecione uma Imagem</label>
+                        <input type="file" name="image">
+                    </div>
+                    <div class="field">
+                        <input type="submit" name="submit" value="Continue para conversar">
+                    </div>
+            </form>
+            <div class="link">Já possui uma conta? <a href="#">Fazer Login</a></div>
+        </section>
+    </div>
+    
+</body>
+</html>
