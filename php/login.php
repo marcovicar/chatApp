1356d1cbeecc0fd1e96650420cb0749e3e942b2d
marcovicar <66976105+marcovicar@users.noreply.github.com>
2022-04-20 11:24:04 -0300

    Status de Online e Offline 100% funcionando

--- php/login.php
+++ php/login.php
@@ -10,9 +10,12 @@
         if(mysqli_num_rows($sql) > 0){ //Se o email e a senha são os mesmos no banco de dados
             $row = mysqli_fetch_assoc($sql);
             $status = "Online";
-            $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
-            $_SESSION['unique_id'] = $row['unique_id']; //Usando a sessão, nós pegamos o unique_id do usuário no outro arquivo php 
+            $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
+            if($sql2){
+                $_SESSION['unique_id'] = $row['unique_id']; //Usando a sessão, nós pegamos o unique_id do usuário no outro arquivo php 
             echo "success";
+            }
+            
         }else{
             echo "Email ou senha incorretos!";
         }
