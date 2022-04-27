4660472ac7833bf07afc61be10d4d3c3899fc5c6
marcovicar <66976105+marcovicar@users.noreply.github.com>
2022-04-27 11:04:04 -0300

    banco de dados versão final - concluida!

--- bd/chatapp.sql
+++ bd/chatapp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08-Abr-2022 às 04:29
--- Versão do servidor: 10.4.18-MariaDB
--- versão do PHP: 8.0.3
+-- Tempo de geração: 27-Abr-2022 às 16:01
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 7.4.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `chatapp`
 --
+
+CREATE OR REPLACE DATABASE chatapp;
+USE chatapp;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `messages`
+--
+
+CREATE TABLE `messages` (
+  `msg_id` int(11) NOT NULL,
+  `outgoing_msg_id` int(255) NOT NULL,
+  `incoming_msg_id` int(255) NOT NULL,
+  `msg` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `messages`
+--
+
+INSERT INTO `messages` (`msg_id`, `outgoing_msg_id`, `incoming_msg_id`, `msg`) VALUES
+(1, 1117633454, 961810301, 'oi'),
+(2, 961810301, 1117633454, 'oi'),
+(3, 1117633454, 961810301, 'tudo bem?'),
+(4, 961810301, 1117633454, 'otimo e vc?'),
+(5, 1117633454, 961810301, 'na paz');
 
 -- --------------------------------------------------------
 
@@ -39,8 +66,24 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `email`, `password`, `img`, `status`) VALUES
+(1, 961810301, 'Marcos', 'Stark', 'marcos@teste.com', 'abacaxi', '1649853098pp.jpg', 'Offline'),
+(2, 1117633454, 'Marcos', 'Rautureau', 'andy@teste.com', 'abacaxi', '1649859014glasses.jpg', 'Offline'),
+(3, 989886484, 'Marcos', 'Wayne', 'marcosw@teste.com', 'abacaxi', '1649859593wow bro.png', 'Offline'),
+(4, 1200252991, 'Thomas', 'Sparky', 'thomas@teste.com', '4b96d5c1ff312eea069ddc760794963d', '1651067350user.png', 'Online');
+
+--
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msg_id`);
 
 --
 -- Índices para tabela `users`
@@ -53,10 +96,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de tabela `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
